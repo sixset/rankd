f5a1b3fb1a3a80c83e644f835e39de1143ccd5f1
D Martyniak <martyniak.dominik@gmail.com>
2023-07-27 18:14:59 +0200

    hexagonal architecture

---
+++ configuration/src/main/java/app/rankd/RankdApplication.java
@@ -0,0 +1,13 @@
+package app.rankd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankdApplication.class, args);
+    }
+
+}
