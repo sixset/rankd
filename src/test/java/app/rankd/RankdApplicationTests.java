8aa09c3bd74c10880204176831b21ba8ca2b6336
D Martyniak <martyniak.dominik@gmail.com>
2023-07-27 17:40:16 +0200

    init

---
+++ src/test/java/app/rankd/RankdApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rankd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
